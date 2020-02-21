a7f89f586331d12da6ce0979722c4827467fae05
zhangyunfeng <zhang4475153@163.com>
2020-02-21 17:21:51 +0800

    update

--- test_dome.py
+++ test_dome.py
@@ -20,5 +20,27 @@ driver.start_activity("apppackage", "appActivity")
 print(driver.current_package)
 print(driver.current_activity)
 
+# 安装app
+# 参致：
+#  app_path:apk路径
+driver.install_app("app_path")
+
+# 卸载app
+# 参数；
+# app_id：应用程序包名
+driver.remove_app("app_id")
+
+# 判断app是否已经安装
+# 参数：
+# app_id：应用程序包名
+# 返回值：
+# 布尔类型，True为安装，False为没有安装
+driver.is_app_installed("app_id")
+
+# 关闭当前操作的app，不会关闭驱动对象
+driver.close_app
+
+# 关闭驱动对象，同时关闭所有关联的app
+driver.quit()
 # 退出
 driver.quit()
