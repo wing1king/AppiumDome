e766bc772bdc3ea7be8d1223777c16c57914ab80
zhangyunfeng <zhang4475153@163.com>
2020-02-22 17:29:21 +0800

    新增高级手势操作

--- test_滑动和拖拽.py
+++ test_滑动和拖拽.py
@@ -41,5 +41,88 @@ driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
 
 driver.swipe(100, 2000, 100, 1000, 2000)
 
+# scroll滑动事件（有惯性）
+# 概念
+# 从一个元素滑动到另一个元素，直到页面自动停止。
+# 方法名
+# 从一个元素滑动到另一个元素，直到页面自动停止
+# 参数：
+# origin_el：滑动开始的元素
+# destination_el：滑动结束的元素
+# driver.scroll(origin_el，destination.el)
+# 示例
+# 从“存储”滑动到“更多”
+# 核心代码
+save_button = driver.find_element_by_xpath("//*[@text='存储]")
+more_button = driver.find_element_by_xpath("//？[@text='更多]")
+driver.scroll(save_button, more_button)
+
+# drag and_drop拖搜事件（不能设置时间，没有惯性）
+# 概念
+# 从一个元素滑动到另一个元素，第二个元素替代第一个元素原本屏幕上的位置。
+# 方法名
+# 从一个元素滑动到另一个元素，第二个元素替代第一个元素原本屏幕上的位置
+# 参数：
+# origin_el：滑动开始的元素
+# destination_el：滑动结束的元素
+# driver.drag_and_drop(origin_e1，destination-el)
+# 示例
+# 将“存储”拖搜到“更多”
+# 核心代码
+save_button = driver.find_element_by_xpath("//*[etext='存储]")
+more_button = driver.find_element_by_xpath("//*[@text='更多]")
+driver.drag_and_drop(save_button, more_button)
+
 # 退出
 driver.quit()
+"""
+小结：
+swipe滑动方式
+方法：
+driver.swipe（起始×坐标，起始y坐标，结束×坐标，结束y坐标，持续时间ms）
+特点：
+参数是坐标点
+持续时间短，惯性大。持续时间长，惯性小
+
+scroll滑动方式
+方法：
+driver.scroll（起始元素，结束元素）
+特点：
+参数是元素
+没有持续时间，有惯性
+
+drag and_drop滑动方式
+方法：
+driver.dragand_drop（起始元素，结束元素）
+特点：
+参数是元素
+没有持续时间，没有惯性
+"""
+
+"""
+滑动和拖拽事件的选择
+滑动和拖拽无非就是考虑是否有“惯性”，以及传递的参数是“元素”还是“坐标”。
+可以分成以下四种情况:
+
+有“惯性”，传入“元素”
+oscroll
+
+无“惯性”，传入“元素”
+drag_and_drop
+
+有“惯性”，传入“坐标”
+swipe，并且设置较短的duration 时间
+
+无“惯性”，传入“坐标”
+swipe，并且设置较长的duration时间
+
+三种方式的选择
+有惯性，传入参数坐标
+swipe，设置较短的持续时间
+有惯性，传入参数元素
+scroll
+无惯性，传入参数坐标
+swipe，设置较长的持续时间
+无惯性，传入参数元素
+drag_and_drop
+"""
---
+++ test_高级手势.py
@@ -0,0 +1,170 @@
+"""
+高级手势
+1.能够使用代码完成轻敲手势
+2.能够使用代码完成按下手势
+3.能够使用代码完成抬起手势
+4.能够使用代码完成等待操作
+5.能够使用代码完成长按手势
+6.能够使用代码完成手指移动操作
+一.高级手势TouchAction
+应用场景
+TouchAction可以实现一些针对手势的操作，比如滑动、长按、拖动等。
+我们可以将这些基本手势组合成一个相对复杂的手势。比如，我们解锁手机或者一些应用软件都有手势解锁的这种方式。
+使用步骤
+1.创建TouchAction对象
+2.通过对象调用想执行的手势
+3.通过perform()执行动作
+注意点
+所有手势都要通过执行perform()函数才会运行。
+"""
+import time
+from appium import webdriver
+from appium.webdriver.common.touch_action import TouchAction
+
+desired_caps = {
+    'platformName': 'Android',
+    'platformVersion': '5.1',
+    'appPackage': 'com.android.settings',
+    'appActivity': '.Settings',
+    'deviceName': '127.0.0.1:62001',
+    # 默认中文无效，需添加 unicodekeyboard，resetkeyboard两个参数
+    'unicodekeyboard': 'True',
+    'resetkeyboard': 'True'}
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+
+# 手指轻敲操作【掌握】
+# 应用场景
+# 模拟手指对某个元素或坐标按下并快速抬起。比如，固定点击（100，100）的位置。
+# 方法名
+# 模拟手指对元素域坐标的轻触操作
+# 参数：
+# element：元素
+# x：×继标#y:y坐标
+# TouchAction（driver）.tap（elementaNone，x=None，y=None）.perform）
+# 示例
+# 1.打开《设置》
+# 2.轻敲"WLAN”
+
+# 核心代码
+# 1.创建touchaction对象
+# 2.调用想要执行的动作
+el = driver.find_element_by_xpath("//*[contains（@text，'WLAN）]")
+# 3.使用perform执行动作
+TouchAction(driver).tap(el).perform()
+# 通过坐标传参    count=2 为点击次数
+TouchAction(driver).tap(x=100, y=200, count=2).perform()
+
+# 按下和抬起操作【掌握】
+# 应用场景
+# 模拟手指一直按下，模拟手指抬凝。可以用来组合成轻敲或长按的操作方法名
+# 模拟手指对元素或坐标的按下操作
+# 参数：
+# e1：元素
+# x：×坐标
+# y: y坐标
+# TouchAction（driver）.press（el=None，X=None，y=None）.perform()
+# 模拟手指对元素或坐标的抬起操作
+# TouchAction（driver）.release().perform()
+# 示例1
+# 使用坐标的形式点击WLAN（650，650），2秒后，按下（650，650）的位置
+# 核心代码
+TouchAction(driver).press(X=650, y=650).perform()
+time.sleep(2)
+TouchAction(driver).press(X=650, y=650).perform()
+# 示例2
+# 使用坐标的形式点击WLAN（650，650），2秒后，按下（650，650）的位置，并抬起
+# 核心代码
+TouchAction(driver).press(X=650, y=650).perform()
+time.sleep(2)
+TouchAction(driver).press(X=650, y=650).release().perform()
+
+# 等待操作【掌握】
+# 应用场景
+# 模拟手指等待，比如按下后博待5秒之后再抬起。
+# 方法名
+# 模拟手指暂定操作
+# 参致：
+# ms：暂停的毫秒数
+# Touchection（driver）.wait（ms=0）.perform()
+# 示例
+# 使用坐标的形式点击 WLAN（650，650），2秒后，按下（650，650）的位置，暂停2秒，并抬起
+# 核心代码
+TouchAction(driver).tap(x=650, y=650).perform()
+time.sleep(2)
+TouchAction(driver).tap(x=650, y=650).perform().wait(2000).release().perform()
+
+# 长按操作【掌握】
+# 应用场景
+# 模拟手指对元素或坐标的长按操作。比如，长按某个按钮弹出菜单。
+# 方法名
+# 模拟手指对元素或坐标的长按操作
+# 参致：
+    # el：元素
+    # x：×坐标
+    # #y:y坐标
+# duration；长按时间，毫秒
+# TouchAction（driver）.long_press（el=None，x=None，y=None，duration=1000）.perform()
+# 示例
+# 使用坐标的形式点击 WLAN（650，650），2秒后，长按（650，650）的位置持续2秒核心代码
+TouchAction(driver).tap(x=400, y=400).perform()
+time.sleep(2)
+TouchAction(driver).long_press(x=400, y=400, duration=2000).release().perform()
+
+
+# 退出
+driver.quit()
+
+"""
+TouchAction
+概念和作用：
+高级手势，可以将小的动作组合成一系列复杂的动作
+·步骤：
+1.创建TouchAction对象
+2.通过对象调用要执行的动作
+3.通过perform进行执行
+
+轻敲
+关键方法：
+tap
+参数：
+可以传入元素
+    使用 element参数
+可以传入坐标
+    使用×和y参数
+多次点击
+    使用count参数
+
+按下
+关键方法：
+press
+参数：
+可以传入元素
+    使用 el参数
+可以传入坐标
+    使用×和y参数
+    
+抬起
+关键方法：
+    release
+    
+等待
+关键方法：。
+wait
+参数：
+    等待的时长（毫秒）
+        使用ms参数    
+
+长按
+关键方法：
+long.press
+参数：
+    可以传入元素
+        使用el参数
+    可以传入坐标
+        使用x和y参数
+    设置持续时间
+        使用duration参数（毫秒）
+额外补充
+    长按<==>按下,等待,抬手
+        
+"""
