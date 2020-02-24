3856d0f692399f403f364a5fcb50f6ea3601dccf
peak.zhang@idreamsky.com <peak.zhang@idreamsky.com>
2020-02-24 12:18:27 +0800

    新增系统弹窗处理和开屏图跳过

--- test_系统弹窗处理.py
+++ test_系统弹窗处理.py
@@ -1,5 +1,20 @@
 """
 系统弹窗处理和跳过首页开屏图
+
+fuction：权限弹窗-始终允许
+args：
+1.传driver
+2.number，判断弹窗次数，默认给5次
+其它：
+WebDriverwait里面0.5s判断一次是否有弹窗，1s超时
+
+for i in range（number）：
+1oc=（"xpath"，"//*[@text=’始终允许]"）try：
+e=Webpriverwait（driver，1，e.5）.until（EC.presence_of_element_located（1oc））
+e.click（）exceRt：
+pass
+
+
 """
 
 import time
